ea4112f13cf6f4cb21f9338a3cd4f43ca66df082
tomkingtech <tomkingtech@gmail.com>
2020-10-13 17:36:36 -0400

    Installed the csv-writer Node module, tested correctAnswers list successfully on chat.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# tunebot
+# **TUNEBOT**  /  Music trivia chatbot for Twitch streams!
+
+Take your Twitch channel chat room and turn it into a fully-automated music trivia experience. Archive who got correct answers during which songs/questions, and take your hands off scoring updates with a fully-automated workflow using Node modules!
+
+---
+
+## Dependencies
+
+- [NodeJS](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [TMI.js](https://tmijs.com/) (JS package for Twitch)
+- Node Modules > [csv-parser](https://www.npmjs.com/package/csv-parser), fs
+
+---
+
+## What makes **TUNEBOT** work?
+
+Here is the basic flow from start-to-finish:
+
+1. Turn on your NodeJS/Express server locally (in your Command Line/Terminal) using tunebot.js.
+
+`node public/javascripts/tunebot.js`
+
+2. Once `tunebot.js` is running, it will capture every message sent to the channel designated in your Twitch API key settings towards the beginning of the file. <mark>**Please keep this data private**!</mark>
+
+3. Using the `fs` Node module, we `.createReadStream()` to take in the data on the .csv file.
+
+4. We `.pipe()` the incoming data into the Node module `csv-parser`, which then allows this data to `.push()` into a variable.
+
+5. The inbound message will then be compared with the active entry being read from the .csv; if answer is correct, the player will be scored according to the order in which correct answers are received.
+
+6. Once the bot/broadcaster types a unique message in chat, the next entry in the .csv is made active, and this cycle continues until the entire .csv has been "parsed."
--- public/javascripts/tunebot.js
+++ public/javascripts/tunebot.js
@@ -72,14 +72,45 @@ client.connect();
 // Bring in the 'csv-parser' and 'fs' NodeJS modules.
 const csv = require('csv-parser');
 const fs = require('fs');
-let results = []; // This declares a JSON array, technically an object-of-objects.
 
-function checkAnswer(currentSong, answer) {
-  // Compare the results.
-  fs.createReadStream('test.csv')
+// Bring in the 'csv-writer' NodeJs module.
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+// Create the initial header information to write to scoreboard.csv
+const csvWriterScoreboard = createCsvWriter({
+  path: 'scoreboard.csv',
+  header: [
+    {id: 'username', title: 'Username'},
+    {id: 'score', title: 'Score'}
+  ]
+});
+
+// Create the initial header information to write to songsguessed.csv
+const csvWriterGuessedBy = createCsvWriter({
+  path: 'songsguessed.csv',
+  header: [
+    {id: 'songnumber', title: 'SongNumber'},
+    {id: 'guessedby1', title: 'GuessedBy1'},
+    {id: 'guessedby2', title: 'GuessedBy2'},
+    {id: 'guessedby3', title: 'GuessedBy3'}
+  ]
+});
+
+let songsList = []; // This is to capture all songs in the .csv file.
+let correctAnswers = []; // This is for all users who answer correctly.
+
+// Empty the correctAnswers array when the next song is queued up.
+function emptyCorrectAnswers() {
+  correctAnswers.length = 0;
+};
+
+// Check to see if the message in Twitch matches the correct answer.
+function checkAnswer(currentSong, answer, usersName) {
+  // Compare the songsList.
+  fs.createReadStream('songs.csv')
   .pipe(csv())
   .on('data', (data) => {
-    results.push(data)
+    songsList.push(data)
   })
   .on('end', () => {
 
@@ -89,7 +120,7 @@ function checkAnswer(currentSong, answer) {
     currentSong = currentSong.toString();
 
     // Assigns the current answer to our checking variable. (The title of the game)
-    answerToCheck = results[`${currentSong}`].Game;
+    answerToCheck = songsList[`${currentSong}`].Game;
     console.log(answerToCheck);
 
     // Turns the input into a string and removes all non-alphanumeric characters.
@@ -97,10 +128,32 @@ function checkAnswer(currentSong, answer) {
       return string.toString().replace(/[^0-9a-zA-Z]/gi, '');
     };
 
+    // Super Mario Bros. 3 or supermariobros3, both are correct.
+
     // If they match, return true. Otherwise, return false.
     if(regexReplace(answer).toLowerCase() === regexReplace(answerToCheck).toLowerCase()) {
+      
       console.log('Answer = ' + true);
-      return true;
+
+      // Place users with correct answers into the correctAnswers array.
+      if(correctAnswers.length === 0) {
+        correctAnswers[0] = usersName;
+        console.log(correctAnswers);
+        console.log("1st correct answer.");
+      }
+      else if(correctAnswers.length === 1) {
+        correctAnswers[1] = usersName;
+        console.log(correctAnswers);
+        console.log("2nd correct answer.");
+      }
+      else if(correctAnswers.length === 2) {
+        correctAnswers[2] = usersName;
+        console.log(correctAnswers);
+        console.log("3rd correct answer.");
+      }
+      else {
+        return;
+      }
     }
     else {
       console.log('Answer = ' + false);
@@ -116,22 +169,28 @@ let songNumber = 1;
 function onMessageHandler (target, context, msg, self) {
   
   // Remove whitespace from chat message
-  const nextMessage = msg.trim();
+  const nextMessage = msg.trim();  // Remove whitespace from ends of the message.
 
   // Log chat message.
   console.log(nextMessage);
 
   // Log username.
-  console.log(context.username);
+  const userAnswering = context.username.toString();
 
-  if(nextMessage == "Next Song") {
+  if(nextMessage === "Next Song" && userAnswering === "PowerToMario") {
+    // Take the correct answers array, and write the list to the current song entry in a .csv
+    emptyCorrectAnswers();
+
     // Increases current song number by 1.
     songNumber++;
     console.log(songNumber);
   }
   else {
     // Checks for matching correct answer, expected value is 'true' or 'false'.
-    console.log(checkAnswer(songNumber, nextMessage));
+    checkAnswer(songNumber, nextMessage, userAnswering);
+    console.log("###");
+    console.log(correctAnswers);
+    console.log("###");
   }
 
 };
